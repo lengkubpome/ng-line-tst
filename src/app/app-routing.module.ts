df23ee51abc61da9f7b971943288aa6f707666b9
lengkubpome <lengkubpome@gmail.com>
2022-05-06 23:54:34 +0700

    setup

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,10 +1,22 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
 
-const routes: Routes = [];
+const routes: Routes = [
+  { path: '', redirectTo: '/member', pathMatch: 'full' },
+  {
+    path: 'member',
+    loadChildren: () =>
+      import('./modules/member/member.module').then((m) => m.MemberModule),
+  },
+  {
+    path: 'price',
+    loadChildren: () =>
+      import('./modules/price/price.module').then((m) => m.PriceModule),
+  },
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
-  exports: [RouterModule]
+  exports: [RouterModule],
 })
-export class AppRoutingModule { }
+export class AppRoutingModule {}
